cc84c7ebe63efdae358decd3382992cae1d89314
guocong <ryp0313@gmail.com>
2021-06-12 12:24:33 -0700

    initialize MERN

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dbUser:wSKugyNPk4fU7f6s@cluster0.ipf2u.mongodb.net/Cluster0?retryWrites=true&w=majority'
+            // 'mongodb://<DBUser>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority'
+            // Make sure this is your own unique string
+}
