4596211f05fdbf310af96cddeb301acfceda8b57
guocong <ryp0313@gmail.com>
2021-06-12 15:53:37 -0700

    finish User Auth

--- app.js
+++ app.js
@@ -2,13 +2,39 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
-app.get("/", (req, res) => res.send("Hello 2 World"));
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json())
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+
+// app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@gmail.com",
+    password: "jim123"
+  })
+  user.save()
+  res.send("Hello 2 World")
+});
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://dbUser:wSKugyNPk4fU7f6s@cluster0.ipf2u.mongodb.net/Cluster0?retryWrites=true&w=majority'
-            // 'mongodb://<DBUser>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority'
-            // Make sure this is your own unique string
+  mongoURI: 'mongodb+srv://dbUser:wSKugyNPk4fU7f6s@cluster0.ipf2u.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  secretOrKey: "secret"
 }
