cbbce2573924ab88afba7f42c225018ec85d6c7f
guocong <ryp0313@gmail.com>
2021-06-13 11:06:24 -0700

    finish tweet

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require('./models/User')
 const passport = require('passport');
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,6 +27,13 @@ require('./config/passport')(passport)
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
